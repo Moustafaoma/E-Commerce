79fcdf41f882a99862b57d1db7cc563c00f9b951
Mostafa <moustafaomar212@gmail.com>
2024-07-14 00:00:35 +0300

    Add 3 Model (Product ,ProductBrand,ProductCategory)

---
+++ E-Commerce.Core/Models/BaseModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Core.Models
+{
+	public class BaseModel
+	{
+        public int ID { get; set; }
+    }
+}
---
+++ E-Commerce.Core/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Core.Models
+{
+	public class Product:BaseModel
+	{
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string PictureUrl { get; set; }
+        //Implemented in Fluent APi
+        //[ForeignKey(nameof(Product.Brand))]
+        public int BrandId { get; set; }
+        public ProductBrand Brand { get; set; }
+		//[ForeignKey(nameof(Product.Category))]
+		public int CategoryId { get; set; }
+
+        public ProductCategory Category { get; set; }
+    }
+}
