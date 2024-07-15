710c123519dad780f19a24ac7f1807961e8bd3f1
Mostafa <moustafaomar212@gmail.com>
2024-07-15 04:03:00 +0300

    Implement Generic Repository

---
+++ E-Commerce.Core/Repository.Contract/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using E_Commerce.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Core.Repository.Contract
+{
+	public interface IGenericRepository<T> where T : BaseModel
+	{
+		 Task<IEnumerable<T>> GetAllAsync();
+		Task<T?> GetByIdAsync(int id);	
+	}
+}
---
+++ E-Commerce.Repository/Repository/GenericRepository.cs
@@ -0,0 +1,28 @@
+﻿using E_Commerce.Core.Models;
+using E_Commerce.Core.Repository.Contract;
+using E_Commerce.Repository.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Repository.Repository
+{
+	public class GenericRepository<T>:IGenericRepository<T> where T : BaseModel
+	{
+		private protected readonly StoreDbContext _context;
+        public GenericRepository(StoreDbContext context)
+        {
+            _context = context;
+        }
+        public async Task <IEnumerable<T>> GetAllAsync()=>
+			 await _context.Set<T>().AsNoTracking().ToListAsync();
+		
+
+		public async Task<T?> GetByIdAsync(int id)=>
+			await _context.Set<T>().FindAsync(id);
+
+	}
+}
